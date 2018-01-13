cc63b27c309b8b3caba6a384fb499548f94686bd
White, Daniel (US - Bristol) <daniel@developerdan.com>
2018-01-13 12:48:18 -0500

    Scope movie-api routes.

--- movie-api/config/routes.rb
+++ movie-api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  namespace :v1 do
-    resources :movies, only: [:index, :create, :show, :update]
+  scope '/movie-api' do
+    namespace :v1 do
+      resources :movies, only: [:index, :create, :show, :update]
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
