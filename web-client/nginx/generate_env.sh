6cc6803432be28aaa1904f7cb797c9ddb1aa8056
White, Daniel (US - Bristol) <daniel@developerdan.com>
2018-01-15 11:21:23 -0500

    Improvements to web-client nginx config for the upstream movie-api.

--- web-client/nginx/generate_env.sh
+++ web-client/nginx/generate_env.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 NGINX_UPSTREAM_FILE="/etc/nginx/conf.d/upstream.conf"
+MOVIE_API_HOST=${MOVIE_API_HOST:='localhost'}
 
 mv /etc/nginx/conf.d/nginx.conf /etc/nginx
 
 cat <<EOF > $NGINX_UPSTREAM_FILE
 upstream movie-api {
-    server localhost:3000;
+    server ${MOVIE_API_HOST}:3000;
 }
 
 EOF
