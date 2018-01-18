442bbb191d639095ea1692f7ff700f6fa75f898e
White, Daniel (US - Bristol) <daniel@developerdan.com>
2018-01-18 12:55:28 -0500

    Disable save/update button to prevent mutiple api calls.

--- web-client/src/app/movies/movie-form/movie-form.component.ts
+++ web-client/src/app/movies/movie-form/movie-form.component.ts
@@ -12,15 +12,19 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 export class MovieFormComponent implements OnInit {
   @Input() confirmationBoxTitle: string;
   @Input() movie: Movie;
-  apiErrors: MovieError = null;
+  apiErrors: MovieError;
+  isSaving: boolean;
 
   constructor(private movieService: MovieService,
               public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
+    this.apiErrors = null;
+    this.isSaving = false;
   }
 
   saveOrUpdateMovie(): void {
+    this.isSaving = true;
     if (this.movie.id) {
       this.movieService.updateMovie(this.movie).then(
         (response) => this.handleResponse(response),
@@ -35,11 +39,23 @@ export class MovieFormComponent implements OnInit {
   }
 
   handleResponse(response) {
+    this.isSaving = false;
     this.activeModal.close(response);
     this.apiErrors = null;
   }
 
   handleError(errors) {
+    this.isSaving = false;
     this.apiErrors = errors as MovieError;
+  }
+
+  action() {
+    if (this.isSaving) {
+      return 'Saving';
+    } else if (this.movie && this.movie.id) {
+      return 'Update';
+    } else {
+      return 'Create';
+    }
   }
 }
