b1ddfd3395a58a7bd436dcadb85d50e9904358bc
White, Daniel (US - Bristol) <daniel@developerdan.com>
2018-01-18 13:38:22 -0500

    Shorten timeout for open movie DB to respond.

--- movie-api/app/clients/open_movie_db.rb
+++ movie-api/app/clients/open_movie_db.rb
@@ -8,7 +8,17 @@ class OpenMovieDb
     return {} if !movie || !movie.valid?
     api_key = Rails.application.secrets.omdb_api_key
     begin
-      response = Faraday.get 'https://www.omdbapi.com', apikey: api_key, t: movie.title, y: movie.release_year
+      response = Faraday.get do |req|
+        req.url 'https://www.omdbapi.com'
+        req.options.timeout = 30           # open/read timeout in seconds
+        req.options.open_timeout = 15      # connection open timeout in seconds
+        req.headers['Content-Type'] = 'application/json'
+        req.params = {
+          apikey: api_key,
+          t: movie.title,
+          y: movie.release_year
+        }
+      end
       parse_response(response)
     rescue # rubocop:disable Style/RescueStandardError
       {}
